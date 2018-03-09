3e78f251af7e478150e239e7a9c989aefd5d3e29
alyokhina-olya <https://github.com/alyokhina-olya/JavaHW>
2018-03-09 04:52:46 +0300

    commit for travis

--- 01.Pool/src/main/java/ru/spbau/mit/alyokhina/LightExecutionException.java
+++ 01.Pool/src/main/java/ru/spbau/mit/alyokhina/LightExecutionException.java
@@ -1,4 +1,5 @@
 package ru.spbau.mit.alyokhina;
+
 /** if in calculating supplier of the LightFuture we caught an exception, we throw LightExecutionException */
 public class LightExecutionException extends Exception {
     public LightExecutionException(String msg) {
